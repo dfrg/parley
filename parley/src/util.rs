acccac3c696c8926ca2ded8aa8359bea0e26b86d
Nico Burns <nico@nicoburns.com>
2024-05-30 16:12:29 +0100

    Remove commented code

--- parley/src/util.rs
+++ parley/src/util.rs
@@ -10,27 +10,3 @@ pub fn nearly_eq(x: f32, y: f32) -> bool {
 pub fn nearly_zero(x: f32) -> bool {
     nearly_eq(x, 0.)
 }
-
-// pub enum IterDirection {
-//     Forward,
-//     Backward,
-// }
-
-// impl IterDirection {
-//     pub fn from_is_forwards(is_forwards: bool) -> Self {
-//         if is_forwards {
-//             Self::Forward
-//         } else {
-//             Self::Backward
-//         }
-//     }
-// }
-
-// #[inline]
-// pub fn iter_dyn_direction<T, I: DoubleEndedIterator<Item = T>>(iterator: impl Into<I>, dir: IterDirection, cb: impl FnMut(T)) {
-//     let iterator = iterator.into();
-//     match dir {
-//         IterDirection::Forward => iterator.for_each(cb),
-//         IterDirection::Backward => iterator.rev().for_each(cb),
-//     }
-// }
